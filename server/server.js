041f7bb2813d5259832b1000dea98c2ca1b10fed
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-05-24 21:07:13 +0530

    work on deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => res.end("Hello Attendx-AI"));
 
