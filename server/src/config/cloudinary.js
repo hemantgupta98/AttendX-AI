7737501d2f4735110362b2363a56e85bf7a73181
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-06-05 16:26:59 +0530

    add api key in env

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.Cloud_nameloud_name,
-  api_key: process.env.API_Key,
-  api_secret: process.env.API_Secret,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
