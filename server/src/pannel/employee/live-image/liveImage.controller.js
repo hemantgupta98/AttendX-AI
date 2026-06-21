20bd4345ff58db8bc22bede3afb20498be59fbe4
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-06-21 11:02:40 +0530

    resolve the import issue

--- server/src/pannel/employee/live-image/liveImage.controller.js
+++ server/src/pannel/employee/live-image/liveImage.controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import FormData from "form-data";
 import fs from "fs";
-import { uploadImage as uploadToCloudinary } from "../media/uploadCloudinary.js";
+import { uploadImage as uploadToCloudinary } from "../media/cloudinary.js";
 
 const roleFolders = {
   employee: "live-image/admin",
