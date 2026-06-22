cd4a989a7832c2598546cc9accb3d4120e80acd6
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-06-22 11:44:32 +0530

    work on api routes of live-image config

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import admin from "../pannel/admin/routes/index.js";
 import employee from "../pannel/employee/routes/index.js";
 import student from "../pannel/student/routes/index.js";
+import liveImage from "../pannel/employee/live-image/liveImage.routes.js";
 
 const router = express.Router();
 
 router.use("/admin", admin);
-router.use("/employee", employee);
+router.use("/employee", employee, liveImage);
 router.use("/student", student);
 
 export default router;
